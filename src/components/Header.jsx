1fa5def9326fb8ed7647d45d46e6912f6600c371
pheadlessg <seankfisher@gmail.com>
2019-08-21 12:23:54 -0700

    bones of site are there, adding more styling and content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link } from 'react-scroll';
+import { Container, Row, Col } from 'reactstrap';
+import '../css/App.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Ashley Duffy</h1>
-      <h2>Wordsmith, singer of songs, drinker of gin</h2>
+    <div className="header">
+      <Container>
+        <Row className="title">
+          <Col>
+            <div className="headertext">Ashley Duffy</div>
+          </Col>
+          <Col>
+            <div className="headertext">Screenwriter</div>
+          </Col>
+        </Row>
+
+        <Row className="linkbar">
+          <Col>
+            <Link
+              className="link"
+              to="about"
+              smooth={true}
+              offset={-130}
+              duration={500}
+              isDynamic={true}
+            >
+              About
+            </Link>
+          </Col>
+          <Col>
+            <Link
+              className="link"
+              to="writing"
+              smooth={true}
+              isDynamic={true}
+              offset={-130}
+              duration={500}
+            >
+              Works
+            </Link>
+          </Col>
+          <Col>
+            <Link
+              className="link"
+              to="contact"
+              smooth={true}
+              offset={-30}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { Link, Element, animateScroll as scroll } from 'react-scroll';
+import '../css/App.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div>
+          <Element name="about">
+            <h1>About Me</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+            volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+            tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+            adipiscing diam donec adipiscing tristique. Sed vulputate mi sit
+            amet mauris commodo quis imperdiet massa. At tempor commodo
+            ullamcorper a lacus vestibulum sed arcu. Massa placerat duis
+            ultricies lacus sed turpis tincidunt. Sit amet nisl purus in. Nisl
+            tincidunt eget nullam non nisi est sit amet. Pretium nibh ipsum
+            consequat nisl.
+          </Element>
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <Element name="writing">
+            <h1>Things What I Have Written</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+            volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+            tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+            adipiscing diam donec adipiscing tristique. Sed vulputate mi sit
+            amet mauris commodo quis imperdiet massa. At tempor commodo
+            ullamcorper a lacus vestibulum sed arcu. Massa placerat duis
+            ultricies lacus sed turpis tincidunt. Sit amet nisl purus in. Nisl
+            tincidunt eget nullam non nisi est sit amet. Pretium nibh ipsum
+            consequat nisl.
+          </Element>
+          <br />
+          <a onClick={this.scrollTop}>To the top!</a>
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <h1>Filler</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+          <Element name="contact">
+            <h1>Contact</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+            volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+            tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+            adipiscing diam donec adipiscing tristique. Sed vulputate mi sit
+            amet mauris commodo quis imperdiet massa. At tempor commodo
+            ullamcorper a lacus vestibulum sed arcu. Massa placerat duis
+            ultricies lacus sed turpis tincidunt. Sit amet nisl purus in. Nisl
+            tincidunt eget nullam non nisi est sit amet. Pretium nibh ipsum
+            consequat nisl.
+          </Element>
+        </div>
+      </div>
+    );
+  }
+  scrollTop = () => {
+    scroll.scrollToTop();
+  };
+}
+
+export default Main;
