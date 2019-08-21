8784a3dba16ef6eeb8e2c192b6a4ca61579d79cc
pheadlessg <seankfisher@gmail.com>
2019-08-20 19:07:20 -0700

    added react scroll and lorem ispem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,170 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from 'react-scroll';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return <div className="App">Here's the application</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Link
+          activeClass="active"
+          to="test1"
+          // spy={true}
+          smooth={true}
+          // offset={50}
+          duration={500}
+        >
+          Test 1
+        </Link>
+        <Header />
+        <h1>Part 1</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 2</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 3</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 4</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <Element name="test1">
+          <h1>Part 5</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
+          volutpat consequat mauris nunc congue nisi vitae suscipit. Ultrices
+          tincidunt arcu non sodales neque sodales ut. Odio aenean sed
+          adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet
+          mauris commodo quis imperdiet massa. At tempor commodo ullamcorper a
+          lacus vestibulum sed arcu. Massa placerat duis ultricies lacus sed
+          turpis tincidunt. Sit amet nisl purus in. Nisl tincidunt eget nullam
+          non nisi est sit amet. Pretium nibh ipsum consequat nisl.
+        </Element>
+        <br />
+        <a onClick={this.scrollTop}>To the top!</a>
+        <h1>Part 6</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 7</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 8</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 9</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 10</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 11</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 12</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+        <h1>Part 13</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Ultrices tincidunt
+        arcu non sodales neque sodales ut. Odio aenean sed adipiscing diam donec
+        adipiscing tristique. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet massa. At tempor commodo ullamcorper a lacus vestibulum sed
+        arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Sit amet
+        nisl purus in. Nisl tincidunt eget nullam non nisi est sit amet. Pretium
+        nibh ipsum consequat nisl.
+      </div>
+    );
+  }
+  scrollTop = () => {
+    scroll.scrollToTop();
+  };
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Ashley Duffy</h1>
+      <h2>Wordsmith, singer of songs, drinker of gin</h2>
+    </div>
+  );
+};
+
+export default Header;
