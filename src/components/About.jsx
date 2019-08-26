48debef9743f06ce94ad64f86ad2b0dd9200c8f9
pheadlessg <seankfisher@gmail.com>
2019-08-25 17:27:21 -0700

    added About info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,25 +11,37 @@ const About = () => {
         </div>
         <img className="picture" src={pic} alt="" />
         <div className="aboutbody">
-          <h2>Hi. I'm Ash. I write stuff.</h2>
+          <h2>Hi. I'm Ash. I write for TV, Radio and Film.</h2>
+          <h2>Hi. I'm Ash. I write things.</h2>
+          <br />
           <p>
-            From a young age I dreamed of being a screenwriter. At the tender
-            age of three I wrote my first four hour epic screenplay which was
-            immediately turned into a hit movie directed by Val Kilmer and won
-            twenty-eight academy awards.
-            <br />
-            <br />I moved from the mean streets of Manchester in 2019, leaving
-            behind the world of cold rain and gritty, crime-infested streets
-            that I was so used to. Now I live in Long Beach, California - the
-            land of hot sun and gritty, crime-infested streets.
-            <br />
-            <br />
-            I'd also like to add that my husband Sean is almost definitely the
-            best thing ever.
-            <br />
-            <br />
-            Also, gizza job. G'wan. Giz. What'cha doin'? Writin' a sitcom? I can
-            do that, mate. Easy peasy. G'wan. Gizza job.
+            Originally from Manchester, UK I moved to Los Angeles in 2019 to
+            pursue my lifelong dream of working in the entertainment industry.
+          </p>
+          <p>
+            Since I was a kid, I loved writing stories. When I was ten I began
+            writing gritty, kitchen-sink narratives for schoolwork - stories
+            about children sent to work in mines perishing in a gruesome
+            fashion. I remember my mother sitting me down to watch the classics
+            'Whatever Happened to Baby Jane?' and 'Rebecca' when I was a girl
+            and marvelling at the dark comedy that lurked behind the main
+            threads of the story. I wanted to write - to create dramas that
+            would grab viewers attention, bring them into worlds they hadn't
+            seen and let them experience amazing things.
+          </p>
+          <p>
+            In 2012 I finally decided to follow my passion and dedicated myself
+            to an MA in Television and Radio Scriptwriting at the University of
+            Salford.
+          </p>
+          <p>
+            After some persistence and a lot of luck, I managed to contact one
+            of my most-admired TV scriptwriters, Debbie Horsfield (Cutting It,
+            Poldark) who agreed to mentor me throughout the course. You can read
+            my final project - the first two episodes of a six-part drama about
+            an enigmatic cult leader who gains control of a small town in the
+            North of England - in the Works section of this site, along with
+            other selections of my writing.
           </p>
         </div>
       </Element>
